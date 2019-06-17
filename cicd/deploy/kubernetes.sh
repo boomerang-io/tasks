40b1c6da38f639bbd0d1972a356df8e54ff2f18f
Tyson Lawrie <twlawrie@us.ibm.com>
2019-06-17 16:36:53 +1000

    :rocket: further work on the new CICD worker and start of Node mode

--- cicd/deploy/kubernetes.sh
+++ cicd/deploy/kubernetes.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-# TODO: get the kubectl command line as part of the profile
 KUBE_HOME=/opt/bin
 KUBE_CLI=$KUBE_HOME/kubectl
-
 KUBE_FILE=$1
+DEPLOY_KUBE_NAMESPACE=$2
 
-$KUBE_CLI apply -f $KUBE_FILE
+echo "Configuring Kubernetes..."
+KUBE_HOME=/opt/bin
+KUBE_CLI=$KUBE_HOME/kubectl
+KUBE_CLI_VERSION=v1.10.2
+
+# Relies on proxy settings coming through if there is a proxy
+# TODO: Update URL to https://helm.sh/blog/get-helm-sh/
+curl -L https://storage.googleapis.com/kubernetes-release/release/$KUBE_CLI_VERSION/bin/linux/amd64/kubectl -o $KUBE_CLI && chmod +x $KUBE_CLI
+
+KUBE_NAMESPACE=$DEPLOY_KUBE_NAMESPACE
+export KUBE_CLUSTER_HOST=wdc3.cloud.boomerangplatform.net #needed for deploy step
+KUBE_CLUSTER_IP=10.190.20.176
+KUBE_CLUSTER_PORT=8001
+KUBE_TOKEN=***REMOVED***
+
+$KUBE_CLI config set-cluster $KUBE_CLUSTER_HOST --server=https://$KUBE_CLUSTER_IP:$KUBE_CLUSTER_PORT --insecure-skip-tls-verify=true
+$KUBE_CLI config set-context $KUBE_CLUSTER_HOST-context --cluster=$KUBE_CLUSTER_HOST
+$KUBE_CLI config set-credentials $KUBE_CLUSTER_HOST-user --token=$KUBE_TOKEN
+$KUBE_CLI config set-context $KUBE_CLUSTER_HOST-context --user=$KUBE_CLUSTER_HOST-user --namespace=$KUBE_NAMESPACE
+$KUBE_CLI config use-context $KUBE_CLUSTER_HOST-context
+$KUBE_CLI apply -f $1
